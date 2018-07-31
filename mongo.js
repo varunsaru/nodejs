de08b75532a2968cde38df4b99c1964a54235bb1
varunsaru <ravindransarangan2013@gmail.com>
2018-07-31 21:52:56 +0530

    updated

--- mongo.js
+++ mongo.js
@@ -21,7 +21,7 @@ MongoClient.connect(url, function(err, db) {
 });*/
 
 
-var MongoClient = require('mongodb').MongoClient;
+/*var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
@@ -35,6 +35,86 @@ MongoClient.connect(url, function(err, db) {
   dbo.collection("NodeDetails").insertMany(myobj, function(err, res) {
     if (err) throw err;
     console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});*/
+
+/*var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("NodeMusic");
+  var ARRsort = { MusicDirector : "A.R.Rahman"};
+  dbo.collection("NodeDetails").find().sort(ARRsort).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});*/
+/*var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("NodeMusic");
+  //Find all documents in the customers collection:
+  dbo.collection("NodeDetails").find({MusicDirector : "A.R.Rahman"}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});*/
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("NodeMusic");
+//   //Find all documents in the customers collection:
+//   dbo.collection("NodeDetails").find(({MusicDirector : 'A.R.Rahman'},{Singer : 'Unnikrishnan'})).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("NodeMusic");
+//   var myquery = { Film:"Alai Payuthey" };
+//   dbo.collection("NodeDetails").deleteOne(myquery, function(err, obj) {
+//     if (err) throw err;
+//     console.log("1 document deleted");
+//     db.close();
+//   });
+// });
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("NodeMusic");
+//   var myobj =   {SongName:"Evano Oruvan",                    Film:"Alai Paayella",   MusicDirector:"Varun",       Singer:"Swarnalatha"}
+// ;
+//   dbo.collection("NodeDetails").insertOne(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     db.close();
+//   });
+// });
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("NodeMusic");
+  //Find all documents in the customers collection:
+  dbo.collection("NodeDetails").find(({Singer : 'Unnikrishnan'})).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
     db.close();
   });
 });
